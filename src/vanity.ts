038760051399feb4cda8a4636e456a0f294845cb
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-02-18 23:25:54 -0800

    feat: add custom gradient spinner;

--- src/vanity.ts
+++ src/vanity.ts
@@ -2,6 +2,8 @@ import chalk from "chalk";
 import gradient from "gradient-string";
 import ora from "ora";
 
+const print = console.log;
+
 const HEADER = `
   ____                  _            ____  _        _ _        ____  _
  / ___|___  _   _ _ __ | |_ ___ _ __/ ___|| |_ _ __(_| | _____/ ___|| |__   __ _ _ __ _ __
@@ -11,7 +13,39 @@ const HEADER = `
                                                                                      |_|
 `;
 
-const print = console.log;
+const spinnerFrames = ((): any[] => {
+  const symbols = "⠁⠂⠄⡀⡈⡐⡠⣀⣁⣂⣄⣌⣔⣤⣥⣦⣮⣶⣷⣿⡿⠿⢟⠟⡛⠛⠫⢋⠋⠍⡉⠉⠑⠡⢁".split("");
+  const chunkLength = Math.round(Math.sqrt(symbols.length));
+
+  const colourEscapes = gradient([
+    "#0ea5e9",
+    "#f59e0b",
+    "#0ea5e9",
+  ])("%".repeat(chunkLength * 5)).split("%");
+
+
+  const colours = [];
+  for (let i = 0; i < colourEscapes.length; i += 2) {
+    const pre = colourEscapes[i];
+    const post = colourEscapes[i + 1] ?? '';
+    colours.push((s: string) => {
+      return `${pre}${s}${post}`;
+    });
+  }
+
+  let index = 0;
+  return colours.flatMap((c, cIdx, arr) => {
+    const coloured = [];
+
+    const chunkLengthCorrected = cIdx >= arr.length - 1 ? symbols.length - index : chunkLength;
+    for (let i = 0; i < chunkLengthCorrected; i++) {
+      coloured.push(c(symbols[index]));
+      index = (index + 1) % symbols.length;
+    }
+
+    return coloured;
+  });
+})();
 
 export function error(msg: string) {
   print("🚨", chalk.bold.red(msg));
@@ -37,8 +71,8 @@ export function createSpinner(message: string) {
   return ora({
     text: message,
     spinner: {
-      frames: ['|', '-', '0'],
-      interval: 300
+      frames: spinnerFrames,
+      interval: 50
     }
   });
 }
